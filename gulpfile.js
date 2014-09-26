cf61933cf51a3d1d45fcf386751fe456e2ce2ebf
Nuria Ruiz <nuria@wikimedia.org>
2014-09-26 13:24:41 -0700

    Removing gulp-clean from build
    We do not need to read files before removing them
    
    Change-Id: I16d65daad174c9e7b585bfc6d3a082b636900f99

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var fs = require('fs'),
 var gulp = require('gulp'),
     rjs = require('gulp-requirejs-bundler'),
     concat = require('gulp-concat'),
-    clean = require('gulp-clean'),
     replace = require('gulp-replace'),
     uglify = require('gulp-uglify'),
     htmlreplace = require('gulp-html-replace'),
@@ -33,6 +32,11 @@ that we use later to replace js (and css files) on index.html
 and in the main requirejs bundle
 */
 var rev = require('gulp-rev');
+
+
+//Since deleting files doesn't work on the file contents
+//there's no reason to use a gulp plugin. An excellent opportunity to use a vanilla node module.
+var del = require('del');
 
 // Config
 var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require.config.js') + '; require;'),
@@ -155,12 +159,15 @@ gulp.task('replace', ['css', 'js'], function () {
 
 
 
-// Removes all files from ./dist/
-gulp.task('clean', function () {
-    return gulp.src('./dist/**/*', {
-            read: false
-        })
-        .pipe(clean());
+// Removes all files from ./dist/ (w/o using streams)
+// you can negate patterns
+gulp.task('clean', function (callback) {
+    del([
+        'dist/*.js',
+        'dist/*.css',
+        'dist/*.html',
+        'dist/*.json'
+    ], callback);
 });
 
 gulp.task('default', ['clean', 'replace', 'lint', 'fonts'], function (callback) {
@@ -172,6 +179,6 @@ gulp.task('default', ['clean', 'replace', 'lint', 'fonts'], function (callback) 
 function logWatcher(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
-gulp.watch('./src/**/*.js', ['js']).on('change', logWatcher);
-gulp.watch('./src/**/*.html', ['html']).on('change', logWatcher);
-gulp.watch('./src/**/*.css', ['css']).on('change', logWatcher);
+gulp.watch('./src/**/*.js', ['replace']).on('change', logWatcher);
+gulp.watch('./src/**/*.html', ['replace']).on('change', logWatcher);
+gulp.watch('./src/**/*.css', ['replace']).on('change', logWatcher);
